de70af5f7767647fb5c6b3a0ac3691f156f6dafc
Mo <mohita111096@gmail.com>
2022-02-14 16:50:19 +0530

    Added GET and POST for customer and pollutant and connected it to MongoDB

---
+++ api/server.js
@@ -0,0 +1,89 @@
+// modules =================================================
+const express = require('express');
+const app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+// set our port
+const port = 3000;
+// configuration ===========================================
+
+// config files
+var db = require('../config/db');
+console.log("connecting--", db);
+mongoose.connect(db.url); //Mongoose connection created
+
+// frontend routes =========================================================
+app.get('/', (req, res) => res.send('Welcome to Tutorialspoint!'));
+
+//defining route
+app.get('/tproute', function (req, res) {
+    res.send('This is routing for the application developed using Node and Express...');
+});
+
+// sample api route
+// grab the customer model we just created
+var Customer = require('./models/customer');
+
+
+app.get('/api/customers', function (req, res) {
+    // use mongoose to get all customers in the database
+    Customer.find(function (err, customers) {
+        // if there is an error retrieving, send the error.
+        // nothing after res.send(err) will execute
+        if (err)
+            res.send(err);
+        res.json(customers); // return all customers in JSON format
+    });
+});
+
+//POST customer details
+app.post('/api/customers/send', function (req, res) {
+    var customer = new Customer(); // create a new instance of the student model
+    customer.customerName = req.body.customerName; // set the student name (comes from the request)
+    customer.emailAddress = req.body.emailAddress;
+    customer.password = req.body.password;
+    customer.save(function (err) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'customer created!' });
+        // console.log(req.body.customerName);
+    });
+});
+
+
+var Pollutant = require('./models/pollutant');
+
+//GET pollutant details
+app.get('/api/pollutants', function (req, res) {
+    // use mongoose to get all customers in the database
+    Pollutant.find(function (err, pollutants) {
+        // if there is an error retrieving, send the error.
+        // nothing after res.send(err) will execute
+        if (err)
+            res.send(err);
+        res.json(pollutants); // return all customers in JSON format
+    });
+});
+
+//POST pollutant details
+app.post('/api/pollutants/send', function (req, res) {
+    var pollutant = new Pollutant(); // create a new instance of the student model
+    pollutant.pollutantName = req.body.pollutantName; // set the student name (comes from the request)
+    pollutant.pollutantConcentration = req.body.pollutantConcentration;
+   
+    pollutant.save(function (err) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'pollutant created!' });
+      
+    });
+});
+
+
+
+
+// startup our app at http://localhost:3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
